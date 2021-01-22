47197bbd16d8528e70bc0fe62ce49ea9c358bdf1
grenudi <idunerg@gmail.com>
2021-01-22 03:14:35 +0300

    stable over HTTPS !

--- src/https/certify.sh
+++ src/https/certify.sh
@@ -1,4 +1,9 @@
+echo "====================| installing certbot|====================";
 sudo snap install core; sudo snap refresh core;
 sudo apt-get remove certbot;
 sudo dnf remove certbot;
 sudo snap install --classic certbot;
+echo "====================| installation DONE |====================";
+
+echo "====================| RUNNING certbot ! |====================";
+certbot certonly --manual --config-dir certbot.d/config/ --work-dir certbot.d/work/ --logs-dir certbot.d/logs 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,52 @@
 "use strict";
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const cfg = require("../package.json").CONFIG;
+
+const appS = express();
+appS.use((req, res) => {
+	res.send('Hello there !');
+});
+
+const app = express();
+app.use((req,res)=>{
+    res.set('location', `https://${cfg.domain.main}`);
+    res.status(302).send();
+})
+
+// Certificate
+const privateKey = fs.readFileSync(__dirname+'/https/certbot.d/config/live/craftclouds.ddns.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync(__dirname+'/https/certbot.d/config/live/craftclouds.ddns.net/cert.pem', 'utf8');
+const ca = fs.readFileSync(__dirname+'/https/certbot.d/config/live/craftclouds.ddns.net/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, appS);
+
+httpServer.listen(cfg.server.http.port, () => {
+	console.log(`HTTP Server running on port ${cfg.server.http.port}`);
+});
+
+httpsServer.listen(cfg.server.https.port, () => {
+	console.log(`HTTPS Server running on port ${cfg.server.https.port}`);
+});
+
+
+
+/*HTTPS ATTEMPT 2
 const Express = require("express");
 const app = Express();
-const cfg = require("../package.json").CONFIG;
+const cfg.server = require("../package.json").CONFIG;
 
 const pubLocation = Express.static("./client-browser");
 app.use(pubLocation);
@@ -10,9 +55,15 @@ app.get("/.well-known/acme-challenge/DSxUs3P938aSB-zhey2xgyH2LZrGYHj4rsXlyvhD2DA
     res.sendFile(__dirname+"/https/cert.txt");
 })
 
-app.listen(cfg.server.port, ()=>{
-    console.warn(`We Rolling on port ${cfg.server.port}`);
+app.listen(cfg.server.server.port, ()=>{
+    console.warn(`We Rolling on port ${cfg.server.server.port}`);
 })
+*/
+
+
+
+
+
 /*
 //HTTPS greenlock setup
 var pkg = require('../package.json');
