3f950390faa596f9c67a84363082bbc73c694b89
grenudi <idunerg@gmail.com>
2021-01-23 19:03:33 +0300

    tmp

--- src/mid/private/private.js
+++ src/mid/private/private.js
@@ -2,7 +2,15 @@ const Express = require('express');
 const app = Express();
 
 app
+.get("/classroom:?examid",(req,res)=>{
+    res.send("AWESOME");
+})
 .get("/classroom",(req,res)=>{
+    //form examid
+    res.send("AWESOME");
+})
+.get("/results:?examid",(req,res)=>{
+    //form examid
     res.send("AWESOME");
 })
 .get("/results",(req,res)=>{
