02ad2e6e6e371dcf362375bcc69af52dbd0fe173
grenudi <idunerg@gmail.com>
2021-01-24 18:49:00 +0300

    stable / /classroom jwt->proc ->(testing)

--- src/mid/private/private.js
+++ src/mid/private/private.js
@@ -3,11 +3,27 @@ const app = Express();
 const cfg = require("../../../package.json").CONFIG;
 const pdb = require("../../pseudoDB/main.js");
 const procjwt = require("../../services/proctoredu.js").jwt;
+const bp = require("body-parser");
+
 
 app
-.post("/api/results",(req,res)=>{
-    console.log(req);
-    console.log("\n\npayload\n\n",req.body);
+.post("/api/report",bp(),(req,res)=>{
+    const reqSecret = req.header("X-Api-Key");
+    console.log("\nGET /api/report:\n",req.body);
+
+    if(reqSecret !== pdb.get("proctorSecret")){
+        // res.status(401).send();
+        return 1;
+    }
+    res.send(200);
+
+    pdb.results.add(req.body.id, req.body);
+})
+.get("/teacher",(req,res)=>{
+    res.sendFile(__dirname+"/serve-routed/teacher.html");
+})
+.post("/teacher/:id",(req,res)=>{
+    res.end(JSON.stringify(pdb.sessions.all()));
 })
 .get("/classroom/:examid",(req,res)=>{
     console.log("/classroom?:examid: ",req.cookies);
@@ -23,24 +39,40 @@ app
     const payload = {...req.cookies,
         referer: `https://${cfg.server.domain.main}/results/${req.cookies.id}`,
         url: `https://${cfg.server.domain.main}/classroom/${req.cookies.id}`,
-        api: "https://${cfg.server.domain.main}/api/results"
+        api: "https://${cfg.server.domain.main}/api/report"
     };
     const token = procjwt.enc(payload);
 
-    pdb.sessions.add(req.cookies.id, new Date().getTime());
+    pdb.sessions.add(req.cookies.id, token);
 
     console.log("TOKEN: ",token);
     res.set('location', `https://${cfg.serverProc.domains[0]}/api/auth/jwt?token=${token}`);
     res.status(308).send();
 })
+.post("/results/:examid",(req,res)=>{
+    console.log("POST /results/:examid", req.params);
+    const examid = req.params.examid;
+    if(pdb.results.isi(examid)){
+        const currentResult = pdb.results.get(examid);
+        res.body = currentResult;
+        res.end(JSON.stringify(currentResult));
+        //send json with results
+    }else{
+        res.status(401).send();
+        return 1;
+    }
+})
 .get("/results/:examid",(req,res)=>{
-    if(pdb.sessions.isi(req.params.examid)){
+    if(pdb.results.isi(req.params.examid)){
+        res.cookie('lastexamid', req.params.examid, { maxAge: 900000, httpsOnly: true });
         res.sendFile(__dirname+"/serve-routed/results.html");
     }else{
         res.set('location', `https://${cfg.server.domain.main}/401.html`);
         res.status(302).send();
     }
 })
+;
+pdb.results.add("examid",{one: "one", subject: "testing examid"});
 // .get("/results",(req,res)=>{
 //     //form examid
 // });
--- src/pseudoDB/main.js
+++ src/pseudoDB/main.js
@@ -1,19 +1,37 @@
+"use strict"
 const sessions = {};
 const sessMaxSize = 20;
 const store = {};
+const results = {};
+const resMaxSize = 20;
 
 module.exports = {
     set: (key,value)=>store[key]=value,
     get: key=>store[key],
     sessions: {
-        add: (session,token)=>{
+        add: (id,token)=>{
             const sessKeysRa = Object.getOwnPropertyNames(sessions);
             const sessSize = sessKeysRa.length;
             if(sessSize > sessMaxSize) delete(sessions[sessKeysRa[0]]);
-            sessions[session] = token;
+            sessions[id] = token;
         },
+        get: id=>sessions[id],
         isi: session=>{
             return sessions[session] ? true : false;
-        }
+        },
+        all: ()=>sessions
+    },
+    results: {
+        add: (id,result)=>{
+            const resRa = Object.getOwnPropertyNames(results);
+            const resRaSize = resRa.length;
+            if(resRaSize > resMaxSize) delete(results[resRa[0]]);
+            results[id] = result;
+        },
+        get: id=>results[id],
+        isi: id=>{
+            return results[id] ? true : false;
+        },
+        all: ()=>results
     }
 }
